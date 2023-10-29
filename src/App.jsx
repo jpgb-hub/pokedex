a06cc2d331003435285d367dabf67216b3626544
jpgb-hub <jpgbonnet@gmail.com>
2023-10-28 21:04:02 -0300

    cardpokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import './App.css'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
+const PokemonGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  gap: 1rem;
+  padding: 1rem;
+`;
+
+const PokemonCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 1rem;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  transition: transform .2s;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const PokemonName = styled.div`
+  margin-top: 0.5rem;
+  text-transform: capitalize;
+  font-weight: bold;
+`;
+
 function App() {
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
@@ -12,24 +42,22 @@ function App() {
 
   return (
     <>
-    <h1>PokeDex</h1>
-    <ul>
-      {pokemons.map((pokemon, index) => (
-        <li key={pokemon.name}>
-          {pokemon.name}
-          <LazyLoadImage
-            alt={pokemon.name}
-            height="96px"
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              index + 1
-            }.png`}
-            width="96px"
-          />
-        </li>
-      ))}
-    </ul>
-  </>
-  )
+      <h1>Bienvenidos al PokeDex</h1>
+      <PokemonGrid>
+        {pokemons.map((pokemon, index) => (
+          <PokemonCard key={pokemon.name}>
+            <LazyLoadImage
+              alt={pokemon.name}
+              height="96px"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+              width="96px"
+            />
+            <PokemonName>{pokemon.name}</PokemonName>
+          </PokemonCard>
+        ))}
+      </PokemonGrid>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/routes/Pokemondetails.jsx
+++ src/routes/Pokemondetails.jsx
@@ -8,7 +8,35 @@ export async function loader({ params }) {
 
 const PokemonDetail = () => {
   const { pokemon } = useLoaderData();
-  return <div>{pokemon.name}</div>;
+
+  const cardStyle = {
+    border: '1px solid #ddd',
+    borderRadius: '8px',
+    padding: '1em',
+    maxWidth: '250px',
+    boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+    textAlign: 'center'
+  };
+
+  const imageStyle = {
+    width: '100px',
+    height: '100px'
+  };
+
+  return (
+    <div style={cardStyle}>
+      <img 
+        style={imageStyle}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} 
+        alt={pokemon.name} 
+      />
+      <h2>{pokemon.name}</h2>
+      <p>Type: {pokemon.type}</p>
+      <p>Height: {pokemon.height}</p>
+      <p>Weight: {pokemon.weight}</p>
+      {/* Add any other details you want to display here */}
+    </div>
+  );
 };
 
 export default PokemonDetail;
